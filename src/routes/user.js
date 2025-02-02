e6a677a3192118cfbe55e08b297c66afd7036754
Shivendra Gupta <gshivendra234@gmail.com>
2025-02-02 16:23:53 -0500

    Added basic feed implementation

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { auth_request } = require('../middlewares/authorize')
 const { ConnectionReqModel } = require('../models/connectionRequest')
+const { userModel } = require('../models/user')
 
 const userRouter = express.Router()
 
@@ -73,6 +74,44 @@ userRouter.get('/v1/user/connections',auth_request,async(req,res)=>{
             message: err.message
         })
     }
+})
+
+userRouter.get('/v1/users/feed',auth_request,async(req,res)=>{
+    const user = req.user
+
+    try{
+        const hiddenUsersFromFeed = await ConnectionReqModel.find({
+            $or:[
+                {toUserId:user},
+                {fromUserId:user}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hidenUsers = new Set()
+
+        hiddenUsersFromFeed.forEach(huser => {
+            console.log(huser.toUserId._id.toString())
+            hidenUsers.add(huser.toUserId._id.toString())
+            hidenUsers.add(huser.fromUserId._id.toString())
+        });
+
+        const arr_users = [...hidenUsers]
+
+        const feed_users = await userModel.find({
+            _id:{$nin : Array.from(arr_users)}
+        }).select("firstName lastName age gender about skills")
+
+        res.send({
+            feed_users
+        })
+
+    }
+    catch(err){
+        res.status(400).send({
+            message:err.message
+        })
+    }
+    
 })
 
 module.exports = {
