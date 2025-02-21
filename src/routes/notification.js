6abe25b75c3c15d5cd22b23d5234f72b61e73d56
Shivendra Gupta <gshivendra234@gmail.com>
2025-02-21 18:32:11 -0500

    Set status of fetched notifications to seen

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -5,41 +5,48 @@ const redisClient = require('../utils/redisConfig')
 
 const notifyRouter = express.Router()
 
-notifyRouter.get('/v1/getMyNotifications',auth_request,async(req,res)=>{
-    try{
-        const user_notification = await notifyModel.find({ userId:req.user})
-        if(!user_notification){
+notifyRouter.get('/v1/getMyNotifications', auth_request, async (req, res) => {
+    try {
+        const user_notification = await notifyModel.find({ userId: req.user ,status:'unseen'})
+        await notifyModel.updateMany({
+            userId: req.user
+        },
+            {
+                $set: { status: 'seen' }
+            }
+        )
+        if (!user_notification) {
             throw new Error("Not found any new Notifications")
         }
-        else{
+        else {
             res.status(200).json({
                 notifications: user_notification
             })
         }
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
             notifications: []
         })
     }
 })
 
-notifyRouter.get('/v1/countMyNotifications',auth_request,async(req,res)=>{
+notifyRouter.get('/v1/countMyNotifications', auth_request, async (req, res) => {
 
     // redisClient
 
-    try{
-        const user_notification_count = await notifyModel.find({ userId:req.user,status:'unseen'})
-        if(!user_notification_count){
+    try {
+        const user_notification_count = await notifyModel.find({ userId: req.user, status: 'unseen' })
+        if (!user_notification_count) {
             throw new Error("Not found any new Notifications")
         }
-        else{
+        else {
             res.status(200).json({
                 notifications: user_notification_count.length
             })
         }
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
             notifications: 0
         })
